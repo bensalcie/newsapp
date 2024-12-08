e63bc6151d77d45d6ed86478c67754ca3b63a6cb
bensalcie <bensalcie@gmail.com>
2024-12-08 16:41:09 +0300

    Implement News Fetching From API

--- lib/pages/news_page.dart
+++ lib/pages/news_page.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:newsapp/models/news_response.dart';
 import 'package:newsapp/models/student.dart';
 
 final students = [
@@ -28,8 +30,20 @@ final students = [
           'https://media.istockphoto.com/id/536810069/photo/the-heart-of-virgin-mary-typical-catholic-image.jpg?s=612x612&w=0&k=20&c=JdBkCPsdSCTEeWPgXLYGt4h75tk34uwQyPpOu50NfJ8=')
 ];
 
-class NewsPage extends StatelessWidget {
+class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
+
+  @override
+  State<NewsPage> createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  List<Articles>? newsList = [];
+  @override
+  void initState() {
+    super.initState();
+    _fetchNews();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,37 +53,34 @@ class NewsPage extends StatelessWidget {
           title: const Text('News App'),
         ),
         body: ListView.builder(
-          itemBuilder: (context, index) =>
-              //  Row(
-              //   children: [
-              //     Image.network(
-              //       students[index].image,
-              //       height: 200,
-              //       width: 200,
-              //       fit: BoxFit.cover,
-              //     ),
-              //     Text(
-              //       students[index].name,
-              //       style: const TextStyle(fontSize: 23, color: Colors.amber),
-              //     ),
-              //   ],
-              // ),
-
-              Column(
+          itemBuilder: (context, index) => Column(
             children: [
               Image.network(
-                students[index].image,
+                newsList?[index].urlToImage ?? '',
                 height: 200,
                 width: double.infinity,
                 fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) => SizedBox.shrink(),
               ),
               Text(
-                students[index].name,
+                newsList?[index].title ?? '',
                 style: const TextStyle(fontSize: 23, color: Colors.amber),
               ),
             ],
           ),
-          itemCount: students.length,
+          itemCount: newsList?.length,
         ));
+  }
+
+  void _fetchNews() async {
+    final dio = Dio();
+    final response = await dio.get(
+        'https://newsapi.org/v2/everything?q=tesla&from=2024-11-08&sortBy=publishedAt&apiKey=8032b416514643b9a80f4779961cdc71');
+
+    final newsResponse = NewsResponse.fromJson(response.data);
+
+    setState(() {
+      newsList = newsResponse.articles;
+    });
   }
 }
